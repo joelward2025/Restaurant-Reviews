54e5ff9074fcc7f03171fe1a9df83b3d76156475
Joel Ward <joelward@cedarville.edu>
2024-08-27 21:58:54 -0400

    Initial Commit

---
+++ Restaurant Reviews/Restaurant_ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_ReviewsApp.swift
+//  Restaurant Reviews
+//
+//  Created by Joel Ward on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
